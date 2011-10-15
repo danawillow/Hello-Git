f726845f40a204990370d48e509a8a7546843352
Dana Hoffman <danawillowhoffman@gmail.com>
2011-10-15 19:31:07 -0400

    Added a new line whoa whoa whoa

--- HelloGit/src/Hello.java
+++ HelloGit/src/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("learning git, super cool");
+		System.out.println("adding another line to this");
 	}
 
 }
